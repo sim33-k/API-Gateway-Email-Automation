72f8ec2d05706cc6eac077a0dba68226d5533c6d
sim33-k <simaakniyaz@gmail.com>
2026-04-29 15:24:22 +0530

    feat: add sensitive output for Jenkins private key in Terraform configuration

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
+
+output "jenkins_private_key_pem" {
+  value     = tls_private_key.jenkins.private_key_pem
+  sensitive = true
+}
