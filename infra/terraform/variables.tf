c9d27af1112b1d9fc51e7335ab4b64ec205ee910
sim33-k <simaakniyaz@gmail.com>
2026-04-29 10:14:59 +0530

    feat: add instance_type variable to Terraform configuration

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,5 @@
 variable "aws_region"     { default = "ap-south-1" }
 variable "your_ip"        {}                            
 variable "ami_id"         {}
+variable "instance_type"  {}
 variable "key_pair_name"  {}
